29ce7f108a459ed86cc14ab9a32ed12191fe68ca
Ogundare Samuel <samuel.ogundare@nextbasket.com>
2024-08-13 07:03:35 +0100

    feat: add args to button component stories

--- src/components/ui/Button.stories.tsx
+++ src/components/ui/Button.stories.tsx
@@ -3,7 +3,10 @@ import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Button> = {
     title: 'Form/Button',
-    component: Button
+    component: Button,
+    args: {
+        // children: 'Button Global Args', 
+    }
 } as Meta<typeof Button>;
 
 export default meta;
@@ -13,3 +16,25 @@ type Story = StoryObj<typeof Button> & (() => JSX.Element);
 export const Primary: Story = () => <Button variant="primary">Primary</Button>;
 export const Secondary: Story = () => <Button variant="secondary">Secondary</Button>;
 export const Danger: Story = () => <Button variant="danger">Danger</Button>;
+
+
+export const PrimaryA = {
+    args: {
+        variant: 'primary',
+        children: 'Primary Args',
+    }
+};
+
+export const SecondaryA = {
+    args: {
+        variant: 'secondary',
+        children: 'Secondary Args',
+    }
+};
+
+export const DangerA = {
+    args: {
+        variant: 'danger',
+        children: 'Danger Args',
+    }
+};
